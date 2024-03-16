7933351c01e2d02e90baad8a1791d99fae0cbf14
Aira Jena <airajena.hack@gmail.com>
2024-03-16 18:51:13 +0530

    feat: Setup initial basic routing using Route groups

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home () {
+  return (
+      <main className="">
+        <div className="">
+           <p>Greedy PI</p>
+        </div>
+      </main>
+  )
+}
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home () {
-  return (
-    <main className="">
-      <div className="">
-         <p>Greedy PI</p>
-      </div>
-    </main>
-  )
-}
