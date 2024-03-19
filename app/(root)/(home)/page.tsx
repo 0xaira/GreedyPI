dc8a7b6f828b4d0ddcb0e0095d3f6629341d12f0
Aira Jena <airajena.hack@gmail.com>
2024-03-19 22:16:15 +0530

    feat: implemented responsive filter tags in home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,10 @@
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
+import HomeFilters from '@/components/home/HomeFilters'
+import Filter from '@/components/shared/Filter'
 import LocalSearchbar from '@/components/shared/search/LocalSearchbar'
+import { Button } from '@/components/ui/button'
+import { HomePageFilters } from '@/constants/filters'
+import Link from 'next/link'
+
 export default function Home () {
   return (
     <>
@@ -12,7 +16,7 @@ export default function Home () {
           </Button>
         </Link>
       </div>
-      <div className="mt-2 flex h-24 justify-between gap-5 max-sm:flex-col sm:items-center">
+      <div className="mt-11 flex h-10 flex-col justify-between gap-2  max-md:flex-row sm:items-start ">
         <LocalSearchbar
           route="/"
           iconPosition="left"
@@ -20,7 +24,13 @@ export default function Home () {
           placeholder="Search questions"
           otherClasses="flex-1"
         />
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
       </div>
+      <HomeFilters />
     </>
   )
 }
--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -14,10 +14,10 @@ const Navbar = () => {
           src="/assets/images/site-logo.svg"
           width={23}
           height={23}
-          alt="DevFlow"
+          alt="GreedyPI"
         />
 
-        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">Dev <span className="text-primary-500">Overflow</span></p>
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">Greedy <span className="text-primary-500">PI</span></p>
       </Link>
 
       <GlobalSearch/>
