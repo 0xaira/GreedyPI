f234f1e98afd5ccb9291699fefad81ce429461c4
Aira Jena <airajena.hack@gmail.com>
2024-03-19 21:08:54 +0530

    feat: built the local search bar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,26 @@
-// import { UserButton } from '@clerk/nextjs'
-
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import LocalSearchbar from '@/components/shared/search/LocalSearchbar'
 export default function Home () {
   return (
-    <div className="h-screen">
-      {/* <UserButton /> */}
-    </div>
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center ">
+        <h1 className="h1-bold text-dark100_light900">All Question</h1>
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+      <div className="mt-2 flex h-24 justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search questions"
+          otherClasses="flex-1"
+        />
+      </div>
+    </>
   )
 }
