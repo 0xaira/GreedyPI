eb01d0bb2f2623c51795b7622d75afd998f82261
Aira Jena <airajena.hack@gmail.com>
2024-03-20 12:58:44 +0530

    feat: built the question card and metrics for home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,6 +5,96 @@ import LocalSearchbar from '@/components/shared/search/LocalSearchbar'
 import { Button } from '@/components/ui/button'
 import { HomePageFilters } from '@/constants/filters'
 import Link from 'next/link'
+import QuestionCard from '@/components/shared/cards/QuestionCard'
+const questions = [
+  {
+    _id: '1',
+    title: 'Best Practices for data fetching in a Next.js app',
+    tags: [
+      { _id: '1', name: 'React' },
+      { _id: '2', name: 'Next.js' }
+    ],
+    author: {
+      _id: 'a1',
+      name: 'Aira',
+      picture: 'url/to/picture1.jpg'
+    },
+    votes: 10,
+    answers: [
+      {
+        /* answer object */
+      },
+      {
+        /* answer object */
+      }
+    ], // Assuming object structure for answers
+    views: 100,
+    createdAt: new Date('1998-08-01T18:30:00.000Z')
+  },
+  {
+    _id: '2',
+    title: 'Redux toolkit Not Updating State as Expected',
+    tags: [{ _id: '1', name: 'React' }],
+    author: {
+      _id: 'a2',
+      name: 'Sounak',
+      picture: 'url/to/picture2.jpg'
+    },
+    votes: 25,
+    answers: [
+      {
+        /* answer object */
+      },
+      {
+        /* answer object */
+      }
+    ], // Assuming object structure for answers
+    views: 1000,
+    createdAt: new Date('2021-08-01T18:30:00.000Z')
+  },
+  {
+    _id: '3',
+    title: 'How do I use express as a custom server in NextJS',
+    tags: [{ _id: '1', name: 'Next.js' }],
+    author: {
+      _id: 'a3',
+      name: 'Priyam',
+      picture: 'url/to/picture3.jpg'
+    },
+    votes: 1,
+    answers: [
+      {
+        /* answer object */
+      },
+      {
+        /* answer object */
+      }
+    ], // Assuming object structure for answers
+    views: 10,
+    createdAt: new Date('2023-08-01T18:30:00.000Z')
+  },
+  {
+    _id: '4',
+    title: 'Next js dynamic routing not working',
+    tags: [{ _id: '1', name: 'NextJs' }],
+    author: {
+      _id: 'a4',
+      name: 'Dev',
+      picture: 'url/to/picture4.jpg'
+    },
+    votes: 100000,
+    answers: [
+      {
+        /* answer object */
+      },
+      {
+        /* answer object */
+      }
+    ], // Assuming object structure for answers
+    views: 5000000,
+    createdAt: new Date('2021-08-01T18:30:00.000Z')
+  }
+]
 
 export default function Home () {
   return (
@@ -32,7 +122,27 @@ export default function Home () {
         />
       </div>
       <HomeFilters />
-      <NoResults
+      <div className="mt-10 flex w-full flex-col gap-6 ">
+        {questions.length > 0
+          ? (
+              questions.map((question) => {
+                return (
+              <QuestionCard
+                _id={question._id}
+                key={question._id}
+                title={question.title}
+                tags={question.tags}
+                author={question.author}
+                votes={question.votes}
+                answers={question.answers}
+                views={question.views}
+                createdAt={question.createdAt}
+              />
+                )
+              })
+            )
+          : (
+          <NoResults
             title=" There’s no question to show"
             description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
           discussion. our query could be the next big thing others learn from. Get
@@ -40,6 +150,8 @@ export default function Home () {
             link="/ask-question"
             linkText="Ask a Question"
           />
+            )}
+      </div>
     </>
   )
 }
