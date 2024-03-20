1df79fa42fcc3486166a1fe0154a9b380b42e6d9
Aira Jena <airajena.hack@gmail.com>
2024-03-20 18:09:56 +0530

    feat: built the question form ui and validations

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+import { QuestionSchema } from '@/lib/validations'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
@@ -15,23 +15,19 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: 'Username must be at least 2 characters.'
-  })
-})
-
 const Question = () => {
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof QuestionSchema>>({
+    resolver: zodResolver(QuestionSchema),
     defaultValues: {
-      username: ''
+      title: '',
+      explaination: '',
+      tags: []
     }
   })
 
   // 2. Define a submit handler.
-  function onSubmit (values: z.infer<typeof formSchema>) {
+  function onSubmit (values: z.infer<typeof QuestionSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values)
@@ -39,21 +35,73 @@ const Question = () => {
   return (
     <><div>Question</div><Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                  control={form.control}
-                  name="username"
-                  render={({ field }) => (
-                      <FormItem>
-                          <FormLabel>Username</FormLabel>
-                          <FormControl>
-                              <Input placeholder="shadcn" {...field} />
-                          </FormControl>
-                          <FormDescription>
-                              This is your public display name.
-                          </FormDescription>
-                          <FormMessage />
-                      </FormItem>
-                  )} />
+          <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className=" flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800 ">
+                Question Title
+                <span className="ml-1 text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border "
+                />
+              </FormControl>
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Be specific and imagine you&apos;re asking a question to another
+                person
+              </FormDescription>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className=" flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800 ">
+                Detailed Explaination of your problem
+                <span className="ml-1 text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border "
+                />
+              </FormControl>
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Introduce the problem and expand on what you put in the title. Minimum 20 characters.
+              </FormDescription>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className=" flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800 ">
+                Tags
+                <span className="ml-1 text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border "
+                />
+              </FormControl>
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Add upto 3 tags to describe what your question is about. You need to press enter to add a tag.
+              </FormDescription>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
               <Button type="submit">Submit</Button>
           </form>
       </Form></>
