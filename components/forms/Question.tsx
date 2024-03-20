2cf7663bf454e316e5355ffcf4aae1c30d6601d8
Aira Jena <airajena.hack@gmail.com>
2024-03-20 18:45:46 +0530

    feat: implemented the question editor using TinyMCE

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,4 +1,6 @@
 'use client'
+import React, { useRef } from 'react'
+import { Editor } from '@tinymce/tinymce-react'
 import { QuestionSchema } from '@/lib/validations'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
@@ -17,6 +19,7 @@ import { Input } from '@/components/ui/input'
 
 const Question = () => {
   // 1. Define your form.
+  const editorRef = useRef(null)
   const form = useForm<z.infer<typeof QuestionSchema>>({
     resolver: zodResolver(QuestionSchema),
     defaultValues: {
@@ -33,78 +36,120 @@ const Question = () => {
     console.log(values)
   }
   return (
-    <><div>Question</div><Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+    <>
+      <div>Question</div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className=" flex w-full flex-col">
+                <FormLabel className="paragraph-semibold text-dark400_light800 ">
+                  Question Title
+                  <span className="ml-1 text-primary-500">*</span>
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border "
+                  />
+                </FormControl>
+                <FormDescription className="body-regular mt-2.5 text-light-500">
+                  Be specific and imagine you&apos;re asking a question to
+                  another person
+                </FormDescription>
+                <FormMessage className="text-red-500" />
+              </FormItem>
+            )}
+          />
           <FormField
           control={form.control}
-          name="title"
+          name="explanation"
           render={({ field }) => (
             <FormItem className=" flex w-full flex-col">
               <FormLabel className="paragraph-semibold text-dark400_light800 ">
-                Question Title
+                Detailed Explanation of your problem
                 <span className="ml-1 text-primary-500">*</span>
               </FormLabel>
-              <FormControl>
-                <Input
-                  {...field}
-                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border "
+              <FormControl className="mt-3.5">
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  onInit={(evt, editor) => {
+                    // @ts-ignore
+                    editorRef.current = editor
+                  }}
+                  initialValue=""
+                  init={{
+                    height: 350,
+                    menubar: false,
+                    plugins: [
+                      'advlist',
+                      'autolink',
+                      'lists',
+                      'link',
+                      'image',
+                      'charmap',
+                      'print',
+                      'preview',
+                      'anchor',
+                      'searchreplace',
+                      'visualblocks',
+                      'codesample',
+                      'code',
+                      'fullscreen',
+                      'insertdatetime',
+                      'media',
+                      'table',
+                      'paste',
+                      'code',
+                      'help',
+                      'wordcount'
+                    ],
+                    toolbar:
+                      'undo redo | ' +
+                      'codesample | bold italic backcolor | alignleft aligncenter ' +
+                      'alignright alignjustify | bullist numlist | ' +
+                      'removeformat | help',
+                    content_style: 'body { font-family:Inter; font-size:16px }'
+                  }}
                 />
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
-                Be specific and imagine you&apos;re asking a question to another
-                person
+                Introduce the problem and expand on what you put in the title.
+                Minimum 20 characters.
               </FormDescription>
               <FormMessage className="text-red-500" />
             </FormItem>
           )}
         />
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => (
-            <FormItem className=" flex w-full flex-col">
-              <FormLabel className="paragraph-semibold text-dark400_light800 ">
-                Detailed Explaination of your problem
-                <span className="ml-1 text-primary-500">*</span>
-              </FormLabel>
-              <FormControl>
-                <Input
-                  {...field}
-                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border "
-                />
-              </FormControl>
-              <FormDescription className="body-regular mt-2.5 text-light-500">
-                Introduce the problem and expand on what you put in the title. Minimum 20 characters.
-              </FormDescription>
-              <FormMessage className="text-red-500" />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => (
-            <FormItem className=" flex w-full flex-col">
-              <FormLabel className="paragraph-semibold text-dark400_light800 ">
-                Tags
-                <span className="ml-1 text-primary-500">*</span>
-              </FormLabel>
-              <FormControl>
-                <Input
-                  {...field}
-                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border "
-                />
-              </FormControl>
-              <FormDescription className="body-regular mt-2.5 text-light-500">
-                Add upto 3 tags to describe what your question is about. You need to press enter to add a tag.
-              </FormDescription>
-              <FormMessage className="text-red-500" />
-            </FormItem>
-          )}
-        />
-              <Button type="submit">Submit</Button>
-          </form>
-      </Form></>
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className=" flex w-full flex-col">
+                <FormLabel className="paragraph-semibold text-dark400_light800 ">
+                  Tags
+                  <span className="ml-1 text-primary-500">*</span>
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border "
+                  />
+                </FormControl>
+                <FormDescription className="body-regular mt-2.5 text-light-500">
+                  Add upto 3 tags to describe what your question is about. You
+                  need to press enter to add a tag.
+                </FormDescription>
+                <FormMessage className="text-red-500" />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </>
   )
 }
 
