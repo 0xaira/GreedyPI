24f855c2870e2ba4f0195c29b16fceb3d21a151d
Aira Jena <airajena.hack@gmail.com>
2024-04-15 11:50:53 +0530

    feat: Change the database

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -16,7 +16,7 @@ export const connectToDatabase = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: 'devflow'
+      dbName: 'pi'
     })
 
     isConnected = true
