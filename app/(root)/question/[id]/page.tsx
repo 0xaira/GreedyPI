2cc4a7510c2f4ac996dcea3d52dd738c3c2ba9d1
Aira Jena <airajena.hack@gmail.com>
2024-04-16 23:04:05 +0530

    feat: Integrate create answer action inside answer form

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -7,8 +7,15 @@ import { formatAndDivideNumber, getTimestamp } from '@/lib/utils'
 import ParseHTML from '@/components/shared/ParseHTML'
 import RenderTag from '@/components/shared/RenderTag'
 import Answer from '@/components/forms/Answer'
-
+import { auth } from '@clerk/nextjs'
+import { getUserById } from '@/lib/actions/user.action'
 const Page = async ({ params, searchParams }) => {
+  const { userId: clerkId } = auth()
+  let mongoUser
+
+  if (clerkId) {
+    mongoUser = await getUserById({ userId: clerkId })
+  }
   const result = await getQuestionByID({ questionId: params.id })
   return (
       <>
@@ -69,7 +76,11 @@ const Page = async ({ params, searchParams }) => {
           />
         ))}
       </div>
-      <Answer />
+      <Answer
+        authorId={JSON.stringify(mongoUser._id)}
+        question={result.content}
+        questionId={JSON.stringify(result._id)}
+      />
       </>
   )
 }
