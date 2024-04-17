ef1b87e40506fbe5b5163b5b0fb365a65e859c8e
Aira Jena <airajena.hack@gmail.com>
2024-04-17 09:38:49 +0530

    fix: Fix answer action for creating answers

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -10,7 +10,7 @@ export async function createAnswer (params: CreateAnswerParams) {
     connectToDatabase()
 
     const { content, author, question, path } = params
-    const newAnswer = new Answer({
+    const newAnswer = await Answer.create({
       content,
       author,
       question,
