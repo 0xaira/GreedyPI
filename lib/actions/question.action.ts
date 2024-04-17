502daaae08778bb1a1facf5ddabbbfb5b6896afa
Aira Jena <airajena.hack@gmail.com>
2024-04-17 11:08:47 +0530

    feat: Implement upvoteQuestion function for question voting

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -4,7 +4,7 @@
 import Question from '@/database/question.model'
 import { connectToDatabase } from '../mongoose'
 import Tag from '@/database/tag.model'
-import { CreateQuestionParams, GetQuestionByIdParams, GetQuestionsParams } from './shared.types'
+import { CreateQuestionParams, GetQuestionByIdParams, GetQuestionsParams, QuestionVoteParams } from './shared.types'
 import User from '@/database/user.model'
 import { revalidatePath } from 'next/cache'
 
@@ -92,6 +92,39 @@ export async function getQuestionByID (params: GetQuestionByIdParams) {
   } catch (error) {
     console.log(error)
     throw error
+  }
+}
+
+export async function upvoteQuestion (params: QuestionVoteParams) {
+  try {
+    connectToDatabase()
+    const { questionId, userId, hasupVoted, hasdownVoted, path } = params
+
+    let updateQuery = {}
+
+    if (hasupVoted) {
+      updateQuery = { $pull: { upvotes: userId } }
+    } else if (hasdownVoted) {
+      updateQuery = {
+        $pull: { downvotes: userId },
+        $push: { upvotes: userId }
+      }
+    } else {
+      updateQuery = {
+        $addToSet: { upvotes: userId }
+      }
+    }
+
+    const question = await Question.findByIdAndUpdate(questionId, updateQuery, {
+      new: true
+    })
+
+    if (!question) {
+      throw new Error('Question not found')
+    }
+    revalidatePath(path)
+  } catch (error) {
+    console.log(error)
   }
 }
 
