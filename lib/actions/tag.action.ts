81182f87da28555a8e2805b367c4e0c026b14529
Aira Jena <airajena.hack@gmail.com>
2024-04-17 12:40:33 +0530

    feat: Implement get questions by tag id function in tag actions

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,7 +1,9 @@
 import { connectToDatabase } from '../mongoose'
-import { GetAllTagsParams, GetTopInteractedTagsParams } from './shared.types'
+import { GetAllTagsParams, GetQuestionsByTagIdParams, GetTopInteractedTagsParams } from './shared.types'
 import User from '@/database/user.model'
-import Tag from '@/database/tag.model'
+import Tag, { ITag } from '@/database/tag.model'
+import { FilterQuery } from 'mongoose'
+import Question from '@/database/question.model'
 export async function getTopInteractedTags (params: GetTopInteractedTagsParams) {
   try {
     await connectToDatabase()
@@ -37,5 +39,40 @@ export async function getAllTags (params: GetAllTagsParams) {
     return { tags }
   } catch (error) {
     console.log(error)
+  }
+}
+
+export async function getQuestionsByTagId (params: GetQuestionsByTagIdParams) {
+  try {
+    await connectToDatabase()
+    const { tagId, page = 1, pageSize = 10, filter, searchQuery } = params
+
+    const tagFilter: FilterQuery<ITag> = { _id: tagId }
+
+    const tag = await Tag.findOne(tagFilter).populate({
+      path: 'questions',
+      model: Question,
+      match: searchQuery
+        ? { title: { $regex: searchQuery, $options: 'i' } }
+        : {},
+      options: {
+        sort: { createdAt: -1 }
+      },
+      populate: [
+        { path: 'tags', model: Tag, select: '_id name' },
+        { path: 'author', model: User, select: '_id clerkId name picture' }
+      ]
+    })
+
+    if (!tag) {
+      throw new Error('Tag not found')
+    }
+
+    const questions = tag.questions
+
+    return { tagTitle: tag.name, questions }
+  } catch (error) {
+    console.log(error)
+    throw error
   }
 }
