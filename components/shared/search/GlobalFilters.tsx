5673e270652ff9082264cb0e8c30171cbb3aa6e7
Aira Jena <airajena.hack@gmail.com>
2024-07-10 10:37:47 +0530

    feat: Implement global search with debounce, filters, and result display

--- components/shared/search/GlobalFilters.tsx
+++ components/shared/search/GlobalFilters.tsx
@@ -1,58 +1,112 @@
 'use client'
-import { Button } from '@/components/ui/button'
-import { GlobalSearchFilters } from '@/constants/filters'
-import { formUrlQuery } from '@/lib/utils'
-import { useRouter, useSearchParams } from 'next/navigation'
-import React, { useState } from 'react'
-
-const GlobalFilters = () => {
-  const router = useRouter()
+import React, { useEffect, useState } from 'react'
+import { ReloadIcon } from '@radix-ui/react-icons'
+import { useSearchParams } from 'next/navigation'
+import Link from 'next/link'
+import Image from 'next/image'
+import GlobalFilters from './GlobalFilters'
+import { globalSearch } from '@/lib/actions/general.action'
+const GlobalResult = () => {
   const searchParams = useSearchParams()
+  const [isLoading, setIsLoading] = useState(false)
+  const [result, setResult] = useState([])
 
-  const typeParams = searchParams.get('type')
-  const [active, setActive] = useState(typeParams || '')
+  const global = searchParams.get('global')
+  const type = searchParams.get('type')
 
-  const handleTypeClick = (item: string) => {
-    if (active === item) {
-      setActive('')
-      const newUrl = formUrlQuery({
-        params: searchParams.toString(),
-        key: 'type',
-        value: null
-      })
-      router.push(newUrl, { scroll: false })
-    } else {
-      setActive(item)
-      const newUrl = formUrlQuery({
-        params: searchParams.toString(),
-        key: 'type',
-        value: item.toLowerCase()
-      })
+  useEffect(() => {
+    const fetchResult = async () => {
+      setResult([])
+      setIsLoading(true)
+      try {
+        // Everything Everywhere all at once
+        const res = await globalSearch({ query: global, type })
+        setResult(JSON.parse(res))
+      } catch (error) {
+        console.error(error)
+      } finally {
+        setIsLoading(false)
+      }
+    }
 
-      router.push(newUrl, { scroll: false })
+    if (global) {
+      fetchResult()
+    }
+  }, [global, type])
+
+  const renderLink = (type: string, id: string) => {
+    switch (type) {
+      case 'question':
+        return `/question/${id}`
+      case 'answer':
+        return `/question/${id}`
+      case 'user':
+        return `/profile/${id}`
+      case 'tag':
+        return `/tags/${id}`
+      default:
+        return '/'
     }
   }
 
   return (
-    <div className=" flex items-center gap-5 px-5">
-      <p className=" text-dark400_light900 body-medium">Type:</p>
-      <div className=" flex  gap-3">
-        {GlobalSearchFilters.map((item) => (
-          <Button
-            onClick={() => handleTypeClick(item.value)}
-            key={item.value}
-            className={` light-border-2 small-medium rounded-full px-5 py-2 capitalize dark:text-light-800 dark:hover:text-primary-500 ${
-              active === item.value
-                ? 'bg-primary-500 text-light-900 dark:hover:text-light-900'
-                : 'bg-light-700 text-dark-400 hover:text-primary-500 dark:bg-dark-500'
-            }`}
-          >
-            {item.name}
-          </Button>
-        ))}
+    <div className=" absolute top-full z-10 mt-3 w-full rounded-xl bg-light-800 py-5 shadow-sm dark:bg-dark-400">
+      <GlobalFilters />
+      <div className=" my-5 h-px bg-light-700/50 dark:bg-dark-500/50" />
+      <div className=" space-y-5">
+        <p className=" text-dark400_light900 paragraph-semibold px-5">
+          Top Match
+        </p>
+
+        {isLoading
+          ? (
+          <div className=" flex-center flex-col px-5">
+            <ReloadIcon className=" my-2 size-10 animate-spin text-primary-500" />
+            <p className=" text-dark200_light800 body-regular">
+              Browsing the entire database
+            </p>
+          </div>
+            )
+          : (
+          <div className=" flex flex-col gap-2">
+            {result.length > 0
+              ? (
+                  result.map((item: any, index: number) => (
+                <Link
+                  href={renderLink(item.type, item.id)}
+                  key={item.type + item.id + index}
+                  className=" flex w-full cursor-pointer items-start gap-3 px-5 py-2.5 hover:bg-light-700/50 dark:bg-dark-400/50 "
+                >
+                  <Image
+                    src="/assets/icons/tag.svg"
+                    alt="svg"
+                    width={18}
+                    height={18}
+                    className=" invert-colors mt-1 object-contain"
+                  />
+                  <div className=" flex flex-col">
+                    <p className=" body-medium text-dark200_light800 line-clamp-1 ">
+                      {item.title}
+                    </p>
+                    <p className=" text-light400_light500 small-medium mt-1 font-bold capitalize">
+                      {item.type}
+                    </p>
+                  </div>
+                </Link>
+                  ))
+                )
+              : (
+              <div className=" flex-center flex-col px-5">
+                <p className=" text-dark200_light800 body-regular px-5 py-2.5">
+                  Oops, no result found
+                </p>
+              </div>
+                )}
+          </div>
+            )}
       </div>
     </div>
   )
 }
 
-export default GlobalFilters
+export default GlobalResult
