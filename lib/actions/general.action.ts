dc53fa151eda82df8cb0719698d57c251de90f94
Aira Jena <airajena.hack@gmail.com>
2024-07-10 22:54:06 +0530

    feat: Add user statistics, including badge counts and reputation

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -1,299 +1,100 @@
-/* eslint-disable no-useless-catch */
 'use server'
 import Question from '@/database/question.model'
 import { connectToDatabase } from '../mongoose'
-import Tag from '@/database/tag.model'
-import {
-  CreateQuestionParams,
-  DeleteQuestionParams,
-  EditQuestionParams,
-  GetQuestionByIdParams,
-  GetQuestionsParams,
-  QuestionVoteParams
-} from './shared.types'
+import { SearchParams } from './shared.types'
 import User from '@/database/user.model'
-import { revalidatePath } from 'next/cache'
 import Answer from '@/database/answer.model'
-import Interaction from '@/database/interaction.model'
-import { FilterQuery } from 'mongoose'
+import Tag from '@/database/tag.model'
 
-// Get all questions
-export async function getQuestions (params: GetQuestionsParams) {
-  try {
-    // Connect to DB
-    await connectToDatabase()
-    const { searchQuery, filter, page = 1, pageSize = 10 } = params
+const SearchableTypes = ['question', 'answer', 'tag', 'user']
 
-    // Calculate the number of questions to skip  based on page number and page size
-
-    const skipAmount = (page - 1) * pageSize
-
-    const query: FilterQuery<typeof Question> = {}
-    if (searchQuery) {
-      query.$or = [
-        { title: { $regex: new RegExp(searchQuery, 'i') } },
-        { content: { $regex: new RegExp(searchQuery, 'i') } }
-      ]
-    }
-
-    // Sort
-    let sortOptions = {}
-
-    switch (filter) {
-      case 'newest':
-        sortOptions = { createdAt: -1 }
-        break
-      case 'frequent':
-        sortOptions = { views: -1 }
-        break
-      case 'unanswered':
-        query.answers = { $size: 0 }
-        break
-      default:
-        sortOptions = { createdAt: -1 }
-        break
-    }
-
-    const questions = await Question.find(query)
-      .populate({
-        path: 'tags',
-        model: Tag
-      })
-      .populate({
-        path: 'author',
-        model: User
-      })
-      .skip(skipAmount)
-      .limit(pageSize)
-      .sort(sortOptions)
-
-    const totalQuestions = await Question.countDocuments(query)
-    const isNext = totalQuestions > skipAmount + questions.length
-
-    return { questions, isNext }
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-// Create Question Action
-export async function createQuestions (params: CreateQuestionParams) {
-  try {
-    // connect to DB
-    await connectToDatabase()
-
-    const { title, content, tags, author, path } = params
-
-    // Create a Question
-    const question = await Question.create({
-      title,
-      content,
-      author
-    })
-
-    const tagDocuments = []
-
-    // Create the tags or get them if they already exist
-    for (const tag of tags) {
-      const existingTag = await Tag.findOneAndUpdate(
-        { name: { $regex: new RegExp(`^${tag}$`, 'i') } },
-        { $setOnInsert: { name: tag }, $push: { questions: question._id } },
-        { upsert: true, new: true }
-      )
-      tagDocuments.push(existingTag)
-    }
-
-    await Question.findByIdAndUpdate(question._id, {
-      $push: { tags: { $each: tagDocuments } }
-    })
-
-    // Create an interaction record for the user's ask_question action
-    await Interaction.create({
-      user: author,
-      action: 'ask_question',
-      question: question._id,
-      tags: tagDocuments
-    })
-
-    // Increment author's reputation by +5
-    await User.findByIdAndUpdate(author, { $inc: { reputation: 5 } })
-
-    revalidatePath(path)
-  } catch (err) {
-    console.log(err)
-    throw err
-  }
-}
-
-export async function getQuestionByID (params: GetQuestionByIdParams) {
+export async function globalSearch (params: SearchParams) {
   try {
     await connectToDatabase()
+    const { query, type } = params
+    const regexQuery = { $regex: query, $options: 'i' }
 
-    const { questionId } = params
+    let results = []
 
-    // Get the question data
-    const question = await Question.findById(questionId)
-      .populate({
-        path: 'tags',
+    const modelAndTypes = [
+      {
+        model: Question,
+        searchField: 'title',
+        type: 'question'
+      },
+      {
+        model: User,
+        searchField: 'name',
+        type: 'user'
+      },
+      {
+        model: Answer,
+        searchField: 'content',
+        type: 'answer'
+      },
+      {
         model: Tag,
-        select: '_id name'
-      })
-      .populate({
-        path: 'author',
-        model: 'User',
-        select: '_id clerkId name picture'
-      })
-
-    return question
-  } catch (error) {
-    console.log(error)
-    throw error
-  }
-}
-
-// Upvote Question
-export async function upvoteQuestion (params: QuestionVoteParams) {
-  try {
-    await connectToDatabase()
-    const { questionId, userId, hasupVoted, hasdownVoted, path } = params
-
-    // Update Query to pass
-    let updateQuery = {}
-
-    // if already upvoted ---> remove from upvotes
-    if (hasupVoted) {
-      updateQuery = { $pull: { upvotes: userId } }
-    }
-    // if downvoted ---> remove from downvotes and add to upvote
-    else if (hasdownVoted) {
-      updateQuery = {
-        $pull: { downvotes: userId },
-        $push: { upvotes: userId }
+        searchField: 'name',
+        type: 'tag'
       }
-    }
-    // If did not do anything yet---> just add to upvotes
-    else {
-      updateQuery = {
-        $addToSet: { upvotes: userId }
-      }
-    }
+    ]
 
-    const question = await Question.findByIdAndUpdate(questionId, updateQuery, {
-      new: true
-    })
+    const typeLower = type?.toLowerCase()
 
-    if (!question) {
-      throw new Error('Question not found')
-    }
+    if (!typeLower || !SearchableTypes.includes(typeLower)) {
+      // SEARCH EVERYTHING
+      for (const { model, searchField, type } of modelAndTypes) {
+        const queryResults = await model
+          .find({ [searchField]: regexQuery })
+          .limit(2)
 
-    // Increment author's reputation by +10 for upvoting a question
-    await User.findByIdAndUpdate(userId, {
-      $inc: { reputation: hasupVoted ? -1 : 1 }
-    })
-
-    await User.findByIdAndUpdate(question.author, {
-      $inc: { reputation: hasupVoted ? -10 : 10 }
-    })
-
-    revalidatePath(path)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-// Down Vote Question
-export async function downvoteQuestion (params: QuestionVoteParams) {
-  try {
-    await connectToDatabase()
-
-    const { userId, questionId, hasdownVoted, hasupVoted, path } = params
-
-    let updateQuery = {}
-
-    if (hasupVoted) {
-      updateQuery = {
-        $pull: { upvotes: userId },
-        $push: { downvotes: userId }
-      }
-    } else if (hasdownVoted) {
-      updateQuery = {
-        $pull: { downvotes: userId }
+        results.push(
+          ...queryResults.map((item) => ({
+            title:
+              type === 'answer'
+                ? `Answers containing ${query}`
+                : item[searchField],
+            type,
+            id:
+              type === 'user'
+                ? item.clerkId
+                : type === 'answer'
+                  ? item.question
+                  : item._id
+          }))
+        )
       }
     } else {
-      updateQuery = {
-        $addToSet: { downvotes: userId }
+      // SEARCH IN THE SPECIFIED MODEL TYPE
+      const modelInfo = modelAndTypes.find((item) => item.type === type)
+
+      if (!modelInfo) {
+        throw new Error('invalid search type')
       }
+
+      const queryResults = await modelInfo.model
+        .find({
+          [modelInfo.searchField]: regexQuery
+        })
+        .limit(8)
+
+      results = queryResults.map((item) => ({
+        title:
+          type === 'answer'
+            ? `Answers containing ${query}`
+            : item[modelInfo.searchField],
+        type,
+        id:
+          type === 'user'
+            ? item.clerkId
+            : type === 'answer'
+              ? item.question
+              : item._id
+      }))
     }
-
-    const question = await Question.findByIdAndUpdate(questionId, updateQuery, {
-      new: true
-    })
-
-    if (!question) {
-      throw new Error('Question not found')
-    }
-
-    await User.findByIdAndUpdate(userId, {
-      $inc: { reputation: hasupVoted ? -2 : 2 }
-    })
-    await User.findByIdAndUpdate(question.author, {
-      $inc: { reputation: hasupVoted ? -10 : 10 }
-    })
-
-    revalidatePath(path)
+    return JSON.stringify(results)
   } catch (error) {
-    console.log(error)
-  }
-}
-
-// Delete Question
-export async function deleteQuestion (params: DeleteQuestionParams) {
-  try {
-    connectToDatabase()
-    const { questionId, path } = params
-
-    await Question.deleteOne({ _id: questionId })
-    await Answer.deleteMany({ question: questionId })
-    await Interaction.deleteMany({ question: questionId })
-    await Tag.updateMany(
-      { questions: questionId },
-      { $pull: { questions: questionId } }
-    )
-
-    revalidatePath(path)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-export async function editQuestion (params: EditQuestionParams) {
-  try {
-    connectToDatabase()
-    const { questionId, title, content, path } = params
-
-    const question = await Question.findById(questionId).populate('tags')
-
-    if (!question) {
-      throw new Error('Question not found')
-    }
-
-    question.title = title
-    question.content = content
-    await question.save()
-    revalidatePath(path)
-  } catch (error) {}
-}
-
-export async function getHotQuestions () {
-  try {
-    connectToDatabase()
-    // Get All questions sorted by upvotes and limit by 6
-
-    const hotQuestions = await Question.find({})
-      .sort({ views: -1, upvotes: -1 })
-      .limit(5)
-    return hotQuestions
-  } catch (error) {
-    console.log(error)
+    console.log(`Error fetching global results ${error}`)
+    throw error
   }
 }
